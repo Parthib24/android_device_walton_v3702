eaf62453b68100e8227d2621ffc1c2d3e92a354d
Parthib24 <sirajammunirparthi24@gmail.com>
2017-12-07 18:15:42 +0600

    aosp7: first commit
    Signed-off-by: Parthib24 <sirajammunirparthi24@gmail.com>

--- aosp.mk
+++ aosp.mk
@@ -2,14 +2,14 @@
 PRODUCT_RELEASE_NAME := HT16
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+#$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/Homtom/HT16/device_HT16.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := HT16
-PRODUCT_NAME := lineage_HT16
+PRODUCT_NAME := aosp_HT16
 PRODUCT_BRAND := Homtom
 PRODUCT_MODEL := Homtom HT16
 PRODUCT_MANUFACTURER := Homtom
